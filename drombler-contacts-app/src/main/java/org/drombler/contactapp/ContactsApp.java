caf806cedeaf7503d27f3c06ea6c319000ba7320
Florian Brunner <fbrunnerreg@gmx.ch>
2019-06-05 22:59:02 +0200

    initial import

---
+++ drombler-contacts-app/src/main/java/org/drombler/contactapp/ContactsApp.java
@@ -0,0 +1,32 @@
+package org.drombler.contactapp;
+
+import org.drombler.contactapp.views.PrimaryView;
+import org.drombler.contactapp.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ContactsApp extends MobileApplication {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+    
+    @Override
+    public void init() {
+        addViewFactory(PRIMARY_VIEW, () -> (View) new PrimaryView().getView());
+        addViewFactory(SECONDARY_VIEW, () -> (View) new SecondaryView().getView());
+
+        DrawerManager.buildDrawer(this);
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(ContactsApp.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(ContactsApp.class.getResourceAsStream("/icon.png")));
+    }
+}
---
+++ drombler-contacts-app/src/main/java/org/drombler/contactapp/views/PrimaryView.java
@@ -0,0 +1,7 @@
+package org.drombler.contactapp.views;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class PrimaryView extends FXMLView { 
+
+}
